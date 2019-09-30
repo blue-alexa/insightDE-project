55cb944ac3cf8f927a23377aef57c93fda3405e8
blue-alexa <blue-alexa>
2019-09-29 18:01:29 -0700

    create index

--- es_feed/es_loader.py
+++ es_feed/es_loader.py
@@ -66,6 +66,12 @@ class ESLoader(object):
         # remove index if exist
         if self.conn.indices.exists(index_name):
             self.conn.indices.delete(index_name)
+
+        # create index
+        self.conn.indices.create(index_name)
+        self.conn.cluster.health(wait_for_status="yellow")
+
+        # create mapping
         self.conn.indices.put_mapping(index=index_name, doc_type=type_name, body=mapping)
         self.logger.info(f"Mapping created {mapping}")
 
