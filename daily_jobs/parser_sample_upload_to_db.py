90b0730aa77c88f8686a6a8f98e494eee03c0e15
blue-alexa <blue-alexa>
2019-10-02 13:25:58 -0700

    try encode base64 for parser func

--- daily_jobs/parser_sample_upload_to_db.py
+++ daily_jobs/parser_sample_upload_to_db.py
@@ -1,3 +1,4 @@
+import base64
 from dao import FormParser
 
 sample_parser = "edgar_parser_sample.py"
@@ -6,7 +7,12 @@ form_type = '13f-hr'
 with open(sample_parser, 'rb') as f:
     data = f.read()
 
+encoded_data = base64.b64encode(data)
+
 form_parser = FormParser()
-form_parser.insert_parser(form_type, data)
-code = form_parser.get_parser(form_type)
+form_parser.insert_parser(form_type, encoded_data)
+
+retreived_code = form_parser.get_parser(form_type)
+code = base64.b64decode(retreived_code)
+
 print(code)
