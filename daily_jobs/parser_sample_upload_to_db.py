151c67a9844301f1b6bdaf25f193a9a3cb62bbb9
blue-alexa <blue-alexa>
2019-10-02 13:54:56 -0700

    add base64 encode to parser func

--- daily_jobs/parser_sample_upload_to_db.py
+++ daily_jobs/parser_sample_upload_to_db.py
@@ -1,3 +1,4 @@
+import base64
 from dao import FormParser
 
 sample_parser = "edgar_parser_sample.py"
@@ -5,10 +6,11 @@ form_type = '13f-hr'
 
 with open(sample_parser, 'rb') as f:
     data = f.read()
+encoded = base64.b64encode(data)
 
 form_parser = FormParser()
-form_parser.insert_parser(form_type, data)
+form_parser.insert_parser(form_type, encoded)
 
 retreived_code = form_parser.get_parser(form_type)
-
-print(retreived_code)
+code = base64.decode(retreived_code)
+print(code)
