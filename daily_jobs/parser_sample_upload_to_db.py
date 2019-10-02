d63e8092b733c3d8e55f33838bb5eec30546d12c
blue-alexa <blue-alexa>
2019-10-02 16:48:25 -0700

    add base64 encoding and decoding to parser func

--- daily_jobs/parser_sample_upload_to_db.py
+++ daily_jobs/parser_sample_upload_to_db.py
@@ -6,11 +6,19 @@ form_type = '13f-hr'
 
 with open(sample_parser, 'rb') as f:
     data = f.read()
-encoded = base64.b64encode(data)
 
+# encode func with base64 to avoid potential special character
+encoded = base64.standard_b64encode(data).decode('ascii')
+
+# write parser to db
 form_parser = FormParser()
 form_parser.insert_parser(form_type, encoded)
 
+# read parser from db
 retreived_code = form_parser.get_parser(form_type)
-code = base64.decode(retreived_code)
-print(code)
+b_code = retreived_code.encode()
+code = base64.standard_b64decode(b_code)
+
+# test parser func works
+exec(code)
+print(EdgarFilingParser.doc_pattern)
