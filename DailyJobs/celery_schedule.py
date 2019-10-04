36376e507dad8858d5aad7cdf834879694fb2747
blue-alexa <blue-alexa>
2019-10-03 21:18:09 -0700

    change celery crontab weekday format

--- DailyJobs/celery_schedule.py
+++ DailyJobs/celery_schedule.py
@@ -5,10 +5,10 @@ app.conf.beat_schedule = {
     # Executes every workday at 11 pm.
     'add-every-workday': {
         'task': 'tasks.daily_job',
-        'schedule': crontab(hour=23, minute=00, day_of_week='mon, tue, wed, thu,fri'),
+        'schedule': crontab(hour=23, minute=00, day_of_week='1-5'),
     },
 }
 
 app.conf.timezone = 'US/Pacific'
 
-# celery -A test_celery_schedule beat --loglevel=info
+# celery -A celery_schedule beat --loglevel=info
