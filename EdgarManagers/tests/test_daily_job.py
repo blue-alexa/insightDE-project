7b13da30a26c863d44b7c2e92af139f805e52a55
blue-alexa <blue-alexa>
2019-10-10 22:23:44 -0700

    add system path

--- EdgarManagers/tests/test_daily_job.py
+++ EdgarManagers/tests/test_daily_job.py
@@ -1,9 +1,11 @@
 import sys
+
+sys.path.append("..")
+
 from datetime import datetime
 
 from EdgarManagers.tasks import daily_job
 
-# sys.path.append("..")
 
 download_date = datetime(2019, 10, 9)
 daily_job(download_date)
