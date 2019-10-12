8d9b06ff6785ad43dd5f65a2f44db07fd2e18918
blue-alexa <blue-alexa>
2019-10-12 11:42:21 -0700

    add create mapping method in ES_loader

--- EdgarManagers/EdgarManagers/elasticsearch_dao/es_loader.py
+++ EdgarManagers/EdgarManagers/elasticsearch_dao/es_loader.py
@@ -8,6 +8,9 @@ class ESLoader(object):
         self.conn = elasticsearch.Elasticsearch([ELASTIC_URI])
         self.logger = logging.getLogger("DailyJobs.es_loader")
 
+    def create_mapping(self, index_name, type_name, mapping):
+        self.conn.indices.put_mapping(index=index_name, doc_type=type_name, body=mapping)
+
     def insert(self, index_name, type_name, id, body):
         self.conn.index(index=index_name, doc_type=type_name, id=id, body=body)
         self.logger.info(f"Inserted {id} to ElasticSearch")
